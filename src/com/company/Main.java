345d47d400f2ddbea574c2ff284aca6b1b1205b0
lainessem <0684941@bdeb.qc.ca>
2022-01-21 11:16:09 -0500

    Exercises de 1 à 20 complétés de façon simple avec corrections et call dynamique des methodes.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,14 @@
 package com.company;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Exercise.ex1();
-        Exercise.ex2();
-        Exercise.ex3();
-        Exercise.ex4();
-        Exercise.ex5();
-        Exercise.ex6();
-        Exercise.ex7();
-        Exercise.ex8();
-        Exercise.ex9();
-        Exercise.ex10();
-        Exercise.ex11();
-        Exercise.ex12();
-        Exercise.ex13();
-        Exercise.ex14();
-        Exercise.ex15();
-        Exercise.ex16();
-        Exercise.ex17();
-        Exercise.ex18();
-        Exercise.ex19();
-        Exercise.ex20();
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        Object obj = new Exercise();
+        for (int i = 1; i <= obj.getClass().getDeclaredMethods().length; i++) {
+            obj.getClass().getDeclaredMethod("ex" + i).invoke(obj);
+        }
     }
 }
